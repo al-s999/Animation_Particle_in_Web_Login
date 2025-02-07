958419e67ea5918c2d8ebdd6ddefb7d064ba46fa
al-s999 <ahmadrosyidalfualdi@gmail.com>
2025-02-07 14:42:27 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
 function beginTransition() {
     var bg2=document.getElementById('bg2');
     setTimeout(function() {
