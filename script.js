41857d23ed91a6b54b1579406ae13ea31236a9b9
al-s999 <ahmadrosyidalfualdi@gmail.com>
2025-02-07 13:58:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function beginTransition() {
+    var bg2=document.getElementById('bg2');
+    setTimeout(function() {
+		var mes = document.getElementById('mesOut');
+		mes.style.visibility = "visible";
+		mes.style.opacity = 1;
+		mes.style.top = "40%";
+	}, 1000);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function particlesInit() {
+	var generator = document.getElementById("particleGenerator");
+	var particleCount = 200;
+	for (var i = 0; i < particleCount; i++) {
+		var size = getRandomInt(2, 6);
+		var n = '<div class="particle" style="top:' + getRandomInt(15, 95) + '%; left:' + getRandomInt(5,95) + '%; width:'
+		+ size + 'px; height:' + size + 'px; animation-delay:' + (getRandomInt(0,30)/10) + 's; background-color:rgba('
+		+ getRandomInt(80, 160) + ',' + getRandomInt(185, 255) + ',' + getRandomInt(160, 255) + ',' + (getRandomInt(2, 8)/10) + ');"></div>';
+		console.log("Particle " + i + ": " + n);
+		var node = document.createElement("div");
+		node.innerHTML = n;
+		generator.appendChild(node);
+	}
+}
+
+particlesInit();
+
+
+
